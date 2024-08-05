97992fb6f8c7a4c04e97736cf7b810495c9702ff
Ana Pecic <ana.pecic97@gmail.com>
2024-08-05 10:35:56 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <h1>Theme Creator</h1>
+      <ColorForm />
 
       {initialColors.map((color) => {
         return <Color key={color.id} color={color} />;
@@ -14,4 +15,29 @@ function App() {
   );
 }
 
+
+function ColorForm(){
+  function handleAddColor(){}
+  
+  return <form className="formWrapper" onSubmit={handleAddColor}>
+    <label htmlFor="roleInput">Role</label>
+    <input type="text" id="roleInput" placeholder="add role"></input>
+    
+    <label htmlFor="hexInput">Hex</label>
+    <div>
+    <input type="text" id="hexInput" placeholder="#ff4a11"></input>
+    <input type="color"></input>
+    </div>
+    
+    <label htmlFor="contrastInput">Contrast Text</label>
+    <div>
+    <input type="text" id="contrastInput" placeholder="#000000"></input>
+    <input type="color"></input>
+    </div>
+
+    <button type="submit">ADD COLOR</button>
+  </form>
+}
+
 export default App;
+ 
